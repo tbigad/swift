f16f5e004c54cf953dcc1ae431207945b7cbb29a
tbigad <tbigad@gmail.com>
2019-08-14 22:39:45 +0300

    add editing

--- Note/Note/Operations/DBOperations/SaveNoteDBOperation.swift
+++ Note/Note/Operations/DBOperations/SaveNoteDBOperation.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreData
 
 class SaveNoteDBOperation: BaseDBOperation {
     private let note: Note
@@ -16,12 +17,30 @@ class SaveNoteDBOperation: BaseDBOperation {
     
     func saveNote(){
         backgroundContext.performAndWait {
-            let object = NoteObject(context: backgroundContext) 
-            object.color = self.note.color.toHexString()
-            object.content = self.note.content
-            object.destryDate = self.note.autoRemoveDate
-            object.id = self.note.uid
-            object.title = self.note.title
+            let request = NSFetchRequest <NoteObject>(entityName: UserSettings.shared.modelEntityName)
+            let predicate = NSPredicate(format: "id = %@", self.note.uid)
+            request.predicate = predicate
+            request.returnsObjectsAsFaults = false
+            
+            let object:NoteObject?
+            do {
+                let notes = try backgroundContext.fetch(request)
+                if notes.isEmpty {
+                    object = NoteObject(context: backgroundContext)
+                } else {
+                    object = notes.first
+                }
+                
+                object?.color = self.note.color.toHexString()
+                object?.content = self.note.content
+                object?.destryDate = self.note.autoRemoveDate
+                object?.id = self.note.uid
+                object?.title = self.note.title
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+            
             saveContext(context: backgroundContext)
             finish()
         }
